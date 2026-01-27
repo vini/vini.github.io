7fb82b682dac3f298ce396a07d1b2b36751797d0
Vinicius Sant anna <viniciusosantanna@gmail.com>
2026-01-27 11:44:48 -0300

    base files

---
+++ js/base.js
@@ -0,0 +1,77 @@
+var flkty = new Flickity('.carousel-logos', {
+  wrapAround: true,
+  autoPlay: true,
+  pauseAutoPlayOnHover: false,
+  prevNextButtons: false,
+  pageDots: false,
+  selectedAttraction: 0.01, // quanto menor, mais suave
+  friction: 0.15           // controla desaceleração
+});
+
+
+$(document).ready(function(){
+
+$('a[href^="#"]').on('click', function(e) {
+  e.preventDefault(); // evita o comportamento padrão do link
+
+  var myAttr = this.getAttribute('href');
+  var alvo = $(myAttr);
+  if(alvo.length) {
+    $('html, body').animate({
+      scrollTop: alvo.offset().top - 60 // ajuste o -60 conforme a altura do menu fixo
+    }, {
+      easing: 'swing',
+      duration: 600,
+      complete: function() {
+      }
+    });
+  }
+});
+
+});
+
+const toggle = document.getElementById("themeToggle");
+const html = document.documentElement;
+const status = document.getElementById("status");
+
+toggle.addEventListener("click", () => {
+  const atual = html.getAttribute("data-bs-theme");
+
+  if (atual === "dark") {
+    html.setAttribute("data-bs-theme", "light");
+    toggle.textContent = "🌙";
+    status.textContent = "light";
+  } else {
+    html.setAttribute("data-bs-theme", "dark");
+    toggle.textContent = "☀️";
+    status.textContent = "dark";
+  }
+});
+
+const btn = document.getElementById("btnFullscreen");
+
+btn.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+    btn.innerText = "⛶ Sair da tela cheia";
+  } else {
+    document.exitFullscreen();
+    btn.innerText = "⛶ Tela cheia";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const text = "Welcome to my website ^^";
+  const speed = 80; // velocidade da digitação (ms)
+  let i = 0;
+
+  function typeWriter() {
+    if (i < text.length) {
+      document.getElementById("typewriter").textContent += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+  typeWriter();
+});
